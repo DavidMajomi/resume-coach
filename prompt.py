0b44c42c23918add789f9c27400ae899584e622e
DavidMajomi <77590768+DavidMajomi@users.noreply.github.com>
2024-07-27 11:15:09 -0400

    Added prompting function

---
+++ prompt.py
@@ -0,0 +1,31 @@
+import google.generativeai as genai
+import os
+
+
+
+def write_api_key_to_file(key, file):
+    f = open(file, "w")
+    f.write(key)
+    f.close()
+
+
+def read_api_key_from_file(path_to_file : str) -> str:
+    if os.path.exists(path_to_file):
+        with open(path_to_file, "r") as file:
+            api_key = file.readline()
+    else:
+        raise Exception("File with api key does not exist")
+    
+    return api_key 
+
+
+
+
+def prompt(message):
+    genai.configure(api_key=read_api_key_from_file("gemini_api_key.txt"))
+
+    model = genai.GenerativeModel('gemini-1.5-flash')
+
+    response = model.generate_content(message)
+    
+    return response
